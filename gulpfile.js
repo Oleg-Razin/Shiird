32adfc138819f88ca3dc616b967b3f222585163d
Oleg Razin <razyax@gmail.com>
2020-11-25 01:12:50 +0200

    add section hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,102 +1,290 @@
-var gulp    = require('gulp'),
-    sass    = require('gulp-sass'),
-    browserSync = require('browser-sync'),
-    concat  = require('gulp-concat'),
-    uglify  = require('gulp-uglifyjs'),
-    cleanCss= require('gulp-clean-css'),
-    autoprefixer = require('gulp-autoprefixer'),
-    rename  = require('gulp-rename'),
-    htmlmin      = require('gulp-htmlmin'),
-    del     = require('del');
-    imagemin    = require('gulp-imagemin'), 
-    pngquant    = require('imagemin-pngquant');     
+/* plugins */
+var axe = require('gulp-axe-webdriver'),
+    browsersync = require('browser-sync'),
+    cache = require('gulp-cache'),
+    changed = require('gulp-changed'),
+    cheerio = require('gulp-cheerio'),
+    autoprefixer = require('gulp-autoprefixer');
+    cleanCSS = require('gulp-clean-css'),
+    clone = require('gulp-clone'),
+    concat = require('gulp-concat'),
+    del = require('del'),
+    gulp = require('gulp'),
+    gulpFilter = require('gulp-filter'),
+    gulpIf = require('gulp-if'),
+    imagemin = require('gulp-imagemin'),
+    merge = require('merge-stream'),
+    notify = require('gulp-notify'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    svgSymbols = require('gulp-svg-symbols'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify = require('gulp-uglify'),
+    webp = require('gulp-webp'),
+    zip = require('gulp-zip'),
+    order = require("gulp-order");
 
 
-gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: 'dist'
-        },
-        notify: false
-    });
+/* paths */
+
+var paths = {
+    src: 'src/',
+    dist: 'dist/'
+};
+
+var inputs = {
+    js: paths.src + 'js',
+    css: paths.src + 'css',
+    scss: paths.src + 'scss',
+    img: paths.src + 'img',
+    sprites: paths.src + 'sprites',
+    css_img_path: '../img'
+};
+
+var outputs = {
+    js: paths.dist + 'js',
+    css: paths.dist + 'css',
+    scss: paths.dist + 'scss',
+    img: paths.dist + 'img',
+    sprites: paths.dist + 'sprites',
+    css_img_path: '../img'
+};
+
+// url of your project
+var urlSync = 'esr.local';
+
+/*
+ * Tasks
+ * 1/ compass
+ * 2/ scripts
+ * 3/ images
+ * 4/ sprites
+ * 5/ extend
+ * 6/ styleguide
+ * 7/ zip
+ * 8/ watch
+ * 9/ accessibility check
+ * 
+ */
+
+/* remove print css from concatenation + Concatenate & Minify CSS */
+gulp.task('sass', function () {
+    var filterPrint = gulpFilter(['*', '!print.scss']);
+
+    var all = gulp.src([inputs.scss + '/style.scss',
+    inputs.scss + '/**/*.scss',
+    inputs.scss + '/*.scss'])
+        .pipe(plumber())
+        .pipe(filterPrint)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.init())
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest(outputs.css))
+        .pipe(cleanCSS())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(outputs.css))
+        .pipe(browsersync.reload({ stream: true }))
+        .pipe(notify({ message: 'styles.min.css generated', onLast: true }));
+
+    var print = gulp.src(inputs.scss + '/print.scss')
+        .pipe(plumber())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(outputs.css))
+        .pipe(cleanCSS())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(outputs.css))
+        .pipe(browsersync.reload({ stream: true }))
+        .pipe(notify({ message: 'print.min.css generated', onLast: true }));
+
+    return all, print;
 });
 
-
-
-gulp.task('sass' , function () {
-    return gulp.src('src/scss/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('dist/css'))
-        .pipe(browserSync.reload({stream:true}));
-});
-gulp.task('script' , function () {
+/* Concatenate & Minify JS */
+gulp.task('scripts', function () {
     return gulp.src([
-        'src/js/libs/**/*.js',
-        'src/js/*.js',
-    ])
+        '!' + inputs.js + '/home.js',
+        inputs.js + '/shared/*.js', 
+        inputs.js + '/*.js'
+        ])
+        //manage order
+        .pipe(order([
+            'swiper.min.js',
+            'magnific-popup.min.js',
+            'video.min.js',
+            'global.js',
+        ]))
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest(outputs.js))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('/'))
+        .pipe(gulp.dest(outputs.js))
+        .pipe(browsersync.reload({ stream: true }))
+        .pipe(notify({ message: 'Scripts task complete', onLast: true }));
+});
+
+gulp.task('home', function () {
+    return gulp.src([inputs.js + '/home.js'])
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
         .pipe(concat('home.js'))
-        .pipe(gulp.dest('dist/js'));
-});
-gulp.task('script-jquary' , function () {
-    return gulp.src([
-        'node_modules/jquery/dist/jquery.min.js',
-    ])
-        .pipe(gulp.dest('dist/js'));
-});
-
-gulp.task('img', function() {
-    return gulp.src('src/images/**/*')
-    .pipe(cache(imagemin({
-      interlaced: true,
-      progressive: true,
-      svgoPlugins: [{removeViewBox: false}],
-      use: [pngquant()]
-    })))
-    .pipe(gulp.dest('dist/images'));
+        .pipe(gulp.dest(outputs.js))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('/'))
+        .pipe(gulp.dest(outputs.js))
+        .pipe(browsersync.reload({ stream: true }))
+        .pipe(notify({ message: 'Home Script task complete', onLast: true }));
 });
 
-gulp.task('css-min' , function () {
-    return gulp.src([
-        'dist/css/style.css',
-    ])
-        .pipe(cleanCss())
-        .pipe(concat('style.min.css'))
-        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('dist/css'))
+
+//usefull for Drupal/WordPress projects that already include jquery, list all the scripts to exclude here
+gulp.task('scripts_light', function () {
+    var filterJS = gulpFilter(['**', '!jquery.min.js']);
+    return gulp.src([inputs.js + '/shared/*.js', inputs.js + '/*.js'])
+        //manage order
+        .pipe(order([
+            'swiper.min.js',
+            'magnific-popup.min.js',
+            'video.min.js',
+            'global.js',
+            'home.js',
+        ]))
+        .pipe(plumber())
+        .pipe(filterJS)
+        .pipe(sourcemaps.init())
+        .pipe(concat('scripts_light.js')) //for fabricator
+        .pipe(gulp.dest(outputs.js))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('/'))
+        .pipe(gulp.dest(outputs.js))
+        .pipe(browsersync.reload({ stream: true }))
+        .pipe(notify({ message: 'Scripts light task complete', onLast: true }));
 });
 
-gulp.task('clean', function(done) {
-    del.sync('dist');
-    done();
+/* Optimize images */
+gulp.task('images', function () {
+    var cloneSink = clone.sink();
+
+    return gulp.src([inputs.img + '/**/*', '!' + inputs.img + '/**/*.webp'])
+        .pipe(plumber())
+        .pipe(changed(inputs.img + '/**/*')) //parse only new or updated files
+        .pipe(imagemin([
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.jpegtran({ progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({
+                plugins: [
+                    { removeViewBox: false },
+                    { cleanupIDs: false }
+                ]
+            })
+        ]))
+
+        .pipe(cloneSink)   // clone image
+        .pipe(webp())      // convert cloned image to WebP
+        .pipe(cloneSink.tap())
+        .pipe(gulp.dest(inputs.img));
 });
 
-gulp.task('html', function(){
-    return gulp.src('src/*.html')
-        .pipe(htmlmin())
-        .pipe(gulp.dest('dist/'))
-        .pipe(browserSync.reload({stream:true}));
+/* sprites management 
+ * generate svg file for inline use
+ */
+
+gulp.task('sprites', function () {
+    return gulp.src([inputs.sprites + '/**/*.svg'])
+        .pipe(plumber())
+        .pipe(cheerio({
+            run: function ($) {
+                $('[fill]').removeAttr('fill');
+            },
+            parserOptions: { xmlMode: true }
+        }))
+        .pipe(svgSymbols(
+            {
+                title: '%f',
+                svgClassname: 'a11y_hidden',
+                fontSize: 10
+            }
+        ))
+        .pipe(gulpIf(/[.]svg$/, gulp.dest(inputs.img + '/global')))
+        .pipe(gulpIf(/[.]css$/, gulp.dest(inputs.scss + '/base')))
+        .pipe(notify({ message: 'Sprite generated', onLast: true }));
 });
 
-gulp.task('build-dist', function(done) {
-    var buildJs = gulp.src('src/js/**/*.js')
-        .pipe(gulp.dest('dist/js'));
+/* rename generated css file for sprites */
 
-    var buildImages = gulp.src('src/images/**/*')
-        .pipe(gulp.dest('dist/images'));
-
-    var buildHtml = gulp.src('src/*.html')
-        .pipe(gulp.dest('dist/'));
-
-    done();
-});
-gulp.task('build', gulp.series('clean', 'sass', 'css-min' , 'script','script-jquary', 'build-dist'));
-
-gulp.task('watch', function(){
-    gulp.watch('src/scss/*.scss', gulp.parallel('sass'));
-    gulp.watch('src/libs/**/*.js', gulp.parallel('script'));
-    gulp.watch('src/*.html', gulp.parallel('html'));
-    gulp.watch('src/js/*.js').on("change", browserSync.reload);
-    gulp.watch('dist/*.html').on('change', browserSync.reload);
+gulp.task('rename-sprites', ['sprites'], function () {
+    return gulp.src([inputs.scss + '/base/*.css'])
+        .pipe(plumber())
+        .pipe(rename({
+            basename: '_sprites',
+            extname: '.scss'
+        }))
+        .pipe(gulp.dest(inputs.scss + '/base'))
 });
 
-gulp.task('default', gulp.parallel('html', 'sass', 'css-min', 'script', 'browser-sync','watch'));
+/* delete the useless css file */
+gulp.task('clean-sprites', ['rename-sprites'], function () {
+    return del([inputs.scss + '/base/*.css'], { force: true })
+});
+
+/* Create an archive with the current date containing all files needed
+ * we exlude unecessary files for the customer like scss and sprite source
+ */
+gulp.task('zipDelivery', function () {
+    var date = new Date().toLocaleDateString().replace(/[^0-9]/g, '');
+    return gulp.src(['*.html',
+        inputs.css + '/**',
+        inputs.img + '/**',
+        inputs.js + '/**'], { base: "." })
+        .pipe(plumber())
+        .pipe(zip('delivery-' + date + '.zip'))
+        .pipe(gulp.dest(paths.dist))
+        .pipe(notify({ message: 'Archive generated' }));
+});
+
+/* browsersync */
+// gulp.task('browser-sync', function () {
+//     browsersync.init({
+//         proxy: urlSync, 
+//         port: 3000
+//     });
+// });
+
+// gulp.task('browsersync-reload', function () {
+//     browsersync.reload();
+// });
+
+
+// Watch Files For Changes
+// gulp.task('watch', ['browser-sync'], function (testCB) {
+gulp.task('watch', function (testCB) {
+    gulp.watch(inputs.scss + '/**/*.scss', ['sass']);
+    gulp.watch(inputs.js + '/**/home.js', ['home']);
+    gulp.watch(inputs.js + '/**/!(home.js)*.js', ['scripts']);
+    gulp.watch(inputs.img + '/**/*', ['images']);
+    gulp.watch(inputs.sprites + '/**', ['clean-sprites']);
+});
+
+gulp.task('build-css-js', ['sass','home', 'scripts' ]);
+
+/* accessibility task */
+gulp.task('axe', function (done) {
+    var options = {
+        saveOutputIn: 'a11yResult.json',
+        browser: 'phantomjs',
+        urls: ['*.html']
+    };
+    return axe(options, done);
+});
+
+// Default Task
+gulp.task('default', ['watch']);
+gulp.task('delivery', ['zipDelivery']);
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,23 @@
-$(document).ready(function() {
 
+$(document).ready(function() {
+	var heroSlider = $('.JS-hero-slider');
+
+	if ($(heroSlider).length !== 0){
+		console.log('sdfas')
+		$(heroSlider).each(function(){
+			var sliderContainer = $(this).find('.swiper-container');
+			var slider = new Swiper($(sliderContainer),{
+				loop: true,
+				pagination: {
+					el: '.swiper-pagination',
+					clickable: true,
+        	renderBullet: function (index, className) {
+          	return '<span class="' + className + '">' + 0 + (index + 1) + '</span>';
+        	},
+				}
+			})
+		})
+	}
 });
 
 
