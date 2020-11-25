3ef86211940cf64a634cf1f7b3016e94e69953ee
Oleg Razin <razyax@gmail.com>
2020-11-26 01:00:39 +0200

    add second section, fix slider

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -20,10 +20,16 @@ $(document).ready(function() {
 		console.log('sdfas')
 		$(heroSlider).each(function(){
 			var sliderContainer = $(this).find('.swiper-container');
+			var sliderPagination = $(this).find('.swiper-pagination')
 			var slider = new Swiper($(sliderContainer),{
 				loop: true,
+				speed: 700,
+				autoplay: {
+					delay: 2500,
+					disableOnInteraction: false,
+				},
 				pagination: {
-					el: '.swiper-pagination',
+					el: sliderPagination,
 					clickable: true,
         	renderBullet: function (index, className) {
           	return '<span class="' + className + '">' + 0 + (index + 1) + '</span>';
@@ -32,6 +38,19 @@ $(document).ready(function() {
 			})
 		})
 	}
+
+	var header = $('.o-header')
+	$(window).scroll(function() {
+		var height = $(window).scrollTop();
+
+		if(height > 100){
+		$(header).addClass('o-header--fixed');
+		} else{
+				 /*Если меньше 100px удаляем класс для header*/
+		$('header').removeClass('o-header--fixed');
+		}
+		
+		});
 });
 
 
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,10 +6,16 @@ $(document).ready(function() {
 		console.log('sdfas')
 		$(heroSlider).each(function(){
 			var sliderContainer = $(this).find('.swiper-container');
+			var sliderPagination = $(this).find('.swiper-pagination')
 			var slider = new Swiper($(sliderContainer),{
 				loop: true,
+				speed: 700,
+				autoplay: {
+					delay: 2500,
+					disableOnInteraction: false,
+				},
 				pagination: {
-					el: '.swiper-pagination',
+					el: sliderPagination,
 					clickable: true,
         	renderBullet: function (index, className) {
           	return '<span class="' + className + '">' + 0 + (index + 1) + '</span>';
@@ -18,6 +24,18 @@ $(document).ready(function() {
 			})
 		})
 	}
+
+	var header = $('.o-header')
+	$(window).scroll(function() {
+		var height = $(window).scrollTop();
+
+		if(height > 100){
+		$(header).addClass('o-header--fixed');
+		} else{
+		$('header').removeClass('o-header--fixed');
+		}
+		
+		});
 });
 
 
