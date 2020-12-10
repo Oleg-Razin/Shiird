73d3b2ca10cccd18a2339b5cc54e057a64d2ca31
Oleg Razin <razyax@gmail.com>
2020-12-11 01:50:41 +0200

    add section team

--- src/js/greed.js
+++ src/js/greed.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
 	
-	$('.c-grid').isotope({
+	var gridContainer = $('.c-grid');
+
+	function gridPosition(){
+		var gridItem = $(gridContainer).find('.с-grid__item');
+
+		var gridItemsArray = [];
+		
+		gridItem.each(function(){
+			var gridItemVisible = $(this).css('display');
+			if (gridItemVisible == 'block'){
+				$(this).filter(':nth-child(2n)').addClass('с-grid__item--left');
+				$(this).filter(':nth-child(2n+1)').addClass('с-grid__item--right');
+			}
+		});
+		console.log(gridItemsArray)
+	}
+
+	gridPosition();
+
+	var $grid = $(gridContainer).isotope({
 		layoutMode: 'packery',
 		itemSelector: '.с-grid__item',
 		masonry: {
@@ -8,4 +27,12 @@ $(document).ready(function() {
 		},
 		percentPosition: true
 	});
+
+	$('.filter-button-group').on( 'click', 'button', function() {
+		var filterValue = $(this).attr('data-filter');
+		$grid.isotope({ filter: filterValue });
+		gridPosition();
+	});
+	
+
 });
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
 	var heroSlider = $('.JS-hero-slider');
 
 	if ($(heroSlider).length !== 0){
-		console.log('sdfas')
 		$(heroSlider).each(function(){
 			var sliderContainer = $(this).find('.swiper-container');
 			var sliderPagination = $(this).find('.swiper-pagination')
